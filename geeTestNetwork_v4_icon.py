12cd105506eaf5405debae00586fd6ca084aa380
조준범 <jjb0093@naver.com>
2025-09-23 22:33:20 +0900

    Update geeTestNetwork_v4_icon.py

--- geeTestNetwork_v4_icon.py
+++ geeTestNetwork_v4_icon.py
@@ -46,7 +46,11 @@ class SiamDataset(Dataset):
         path_img1, path_img2 = self.imgPairs[index]
         label = self.labels[index]
 
-        img1, img2 = cv2.imread(f"trainData/icons/{path_img1}", cv2.IMREAD_UNCHANGED), cv2.imread(f"trainData/{path_img2}")
+        img1, img2 = cv2.imread(f"trainDataNew/icons/{path_img1}", cv2.IMREAD_UNCHANGED), cv2.imread(f"trainDataNew/{path_img2}")
+        img1 = cv2.Canny(img1, 300, 400)
+
+        '''
+        img1, img2 = cv2.imread(f"trainData/icons/{path_img1}", cv2.IMREAD_UNCHANGED), cv2.imread(f"trainDataNew/{path_img2}")
         
         b, g, r, a = cv2.split(img1)
 
@@ -54,8 +58,9 @@ class SiamDataset(Dataset):
         b[mask], g[mask], r[mask], a[mask] = 255, 255, 255, 255
 
         img1 = cv2.merge((b, g, r, a))
-
-        img1, img2 = cv2.cvtColor(img1, cv2.COLOR_BGRA2RGB), cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+        '''
+        
+        img1, img2 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB), cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 
         img1, img2 = transform(img1), transform(img2)
         return img1, img2, torch.tensor(label, dtype = torch.float32)
@@ -111,7 +116,7 @@ def evaluate(model, testLoader, device):
     return np.mean(distances)
 
 print(torch.cuda.is_available()) 
-device = torch.device("cuda")
+device = torch.device("cpu")
 
 transform = transforms.Compose([
     transforms.ToPILImage(),
@@ -122,32 +127,45 @@ transform = transforms.Compose([
 
 pos_pairs, neg_pairs = [], []
 
-iconNames = os.listdir("trainData/icons")
+iconNames = os.listdir("trainDataNew/icons")
 for i in range(len(iconNames)):
     print(f"아이콘 {i + 1}/{len(iconNames)} : {iconNames[i]}")
     for k in range(len(iconNames)):
         name = iconNames[k].split(".")[0]
 
-        for data in os.listdir(f"trainData/{name}"):
-            if i == k:
-                pos_pairs.append([iconNames[i], f"{name}/{data}"])
+        for data in os.listdir(f"trainDataNew/datas/{name}"):
+            if(i == k):
+                pos_pairs.append([iconNames[i], f"datas/{name}/{data}"])
             else:
-                neg_pairs.append([iconNames[i], f"{name}/{data}"])
+                neg_pairs.append([iconNames[i], f"datas/{name}/{data}"])
+
+neg_back_pairs = []
+backgroundNames = os.listdir("trainDataNew/backgrounds")
+for i in range(len(iconNames)):
+    for k in range(len(backgroundNames)):
+        print([iconNames[i], f"backgrounds/{backgroundNames[k]}"])
+        neg_back_pairs.append([iconNames[i], f"backgrounds/{backgroundNames[k]}"])
 
 model = SiamNetwork().to(device)
 criterion = Loss()
 optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
-num_epochs = 10
+num_epochs = 100
 avantLoss = float('inf')
+lossCount = 0
+
 for epoch in range(num_epochs):
     neg_sampled = random.sample(neg_pairs, len(pos_pairs))
-    imgPairs = pos_pairs + neg_sampled
-    labels = [0] * len(pos_pairs) + [1] * len(neg_sampled)
+    neg_back_sampled = random.sample(neg_back_pairs, len(neg_back_pairs) // 2)
+
+    imgPairs = pos_pairs + neg_sampled + neg_back_sampled
+    labels = [0] * len(pos_pairs) + [1] * len(neg_sampled) + [1] * len(neg_back_sampled)
 
     combined = list(zip(imgPairs, labels))
     random.shuffle(combined)
     imgPairs[:], labels[:] = zip(*combined)
+
+    print(f"총 이미지 쌍: {len(imgPairs)}, 긍정 쌍: {labels.count(0)}, 부정 쌍: {labels.count(1)}")
 
     dataset = SiamDataset(imgPairs, labels)
     trainLoader = DataLoader(dataset, batch_size = 32, shuffle = True)
@@ -159,3 +177,10 @@ for epoch in range(num_epochs):
     if(avgLoss < avantLoss):
         avantLoss = avgLoss
         torch.save(model.state_dict(), "iconSolverModel.pth")
+        lossCount = 0
+    else:
+        lossCount += 1
+    
+    if(lossCount == 5):
+        print("개선 없음. 학습 종료")
+        break
